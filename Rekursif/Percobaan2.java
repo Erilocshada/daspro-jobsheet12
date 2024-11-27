5c7911e99e16dacfc45d397733e75adaa5d74524
Erilocshada <erilleo23@gmail.com>
2024-11-27 18:33:02 +0700

    Percobaan2

---
+++ Rekursif/Percobaan2.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Percobaan2 {
+    static int hitungPangkat (int x, int y) {
+        if (y == 0) {
+            return (1);
+        }else{
+            return (x*hitungPangkat(x, y-1));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bilangan dihitung :");
+        int bilangan = sc.nextInt();
+        System.out.println("Pangkat: ");
+        int pangakt = sc.nextInt();
+        System.out.println(hitungPangkat(bilangan, pangakt));
+    }
+}
