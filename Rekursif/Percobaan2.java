e281884908b08366ffe8aa684799c5710f0f7ce6
Erilocshada <erilleo23@gmail.com>
2024-11-27 18:33:51 +0700

    Percobaan2Pertanyaan2

--- Rekursif/Percobaan2.java
+++ Rekursif/Percobaan2.java
@@ -2,10 +2,13 @@
 import java.util.Scanner;
 
 public class Percobaan2 {
+    static String deret = "";
     static int hitungPangkat (int x, int y) {
         if (y == 0) {
+            deret += "1";
             return (1);
         }else{
+            deret += x + "x";
             return (x*hitungPangkat(x, y-1));
         }
     }
@@ -16,5 +19,9 @@ public class Percobaan2 {
         System.out.println("Pangkat: ");
         int pangakt = sc.nextInt();
         System.out.println(hitungPangkat(bilangan, pangakt));
+
+        deret = "";
+        int hasil = hitungPangkat(bilangan, pangakt);
+        System.out.println(deret +"="+hasil);
     }
 }
